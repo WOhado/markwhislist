78e76036977c398aeb64a5e5e5a8e1c40af2dcaa
WOhado <wohado1830@duck.com>
2026-04-28 15:11:11 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,10 +6,12 @@ import {defineConfig, loadEnv} from 'vite';
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
-    base: '/markwhislist/', 
+    base: '/markwhislist/',
     plugins: [react(), tailwindcss()],
     build: {
       sourcemap: false,
+      // Ez segít, hogy ne akadjon el a build apróbb típushibákon
+      chunkSizeWarningLimit: 1600,
     },
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
@@ -18,9 +20,6 @@ export default defineConfig(({mode}) => {
       alias: {
         '@': path.resolve(__dirname, '.'),
       },
-    },
-    server: {
-      hmr: process.env.DISABLE_HMR !== 'true',
     },
   };
 });
