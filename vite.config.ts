ffafb2d4b4f3ac4c2db9ec3db8224e8249f0d6fe
WOhado <wohado1830@duck.com>
2026-04-28 22:29:09 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,24 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
-import {defineConfig, loadEnv} from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 
-export default defineConfig(({mode}) => {
+export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
   return {
     base: '/markwhislist/',
     plugins: [react(), tailwindcss()],
     build: {
+      outDir: 'dist',
+      assetsDir: 'assets',
       sourcemap: false,
-      // Ez segít, hogy ne akadjon el a build apróbb típushibákon
-      chunkSizeWarningLimit: 1600,
     },
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
     },
     resolve: {
       alias: {
-        '@': path.resolve(__dirname, '.'),
+        '@': path.resolve(__dirname, './src'),
       },
     },
   };
